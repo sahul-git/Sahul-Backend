f2b231d9dd3b7e0df005f8c300615fa84bc8a369
sahul-git <sahul.chp7480@gmail.com>
2026-02-03 01:04:29 +0530

    connection to mongoATLAS

---
+++ src/index.js
@@ -0,0 +1,51 @@
+// require('dotenv').config(path: './env');
+
+import dotenv from 'dotenv';
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: './env'
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express";
+
+// const app = express();
+
+// (async() => {
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", ()=>{
+//             console.error('Failed to connect to the database');
+//             throw err;
+//         })
+
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`Server is running on port ${process.env.PORT}`);
+//         })
+//     }catch(error){
+//         console.error('Error connecting to the database', err);
+//         throw err;
+//     }
+// })();
